8da01191569c2c978ff3ffa4c250888a9f7bb557
surenderkumar18 <suren.kumar18@gmail.com>
2020-10-05 12:22:35 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CountUseState from './CountUseState';
+import AsyncWithHooks from './AsyncWithHooks';
 import "./style.css";
 
 export default function App() {
   return (
     <div>
      <CountUseState age="40"/>
+     <AsyncWithHooks />
     </div>
   );
 }
---
+++ src/AsyncWithHooks.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const AsyncWithHooks = ( props ) => {
+
+  return (
+    <h1>AsyncWithHooks !!!!!!!!!!</h1>
+  );
+
+}
+
+export default AsyncWithHooks;
